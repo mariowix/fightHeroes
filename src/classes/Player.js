1ca2bb6484186d99e6ce308c4ebb1deb2ec76096
mariomixtega <mariomixtega@Yahoo.com>
2020-05-18 12:06:36 -0500

    Create IPlayer for multiple kind of players

--- src/classes/Player.js
+++ src/classes/Player.js
@@ -1,20 +1,12 @@
 const Phaser = require('phaser');
+const { IPlayer } = require('./IPlayer')
 const { PlayerIndicator } = require('./PlayerIndicator');
  
-class Player extends Phaser.Physics.Arcade.Sprite {
+class Player extends IPlayer {
     constructor(scene, x, y, worldBounds, socketId) {
-        super(scene, x, y, 'entities', 'ship1.png',);
-        scene.add.existing(this);
-        scene.physics.add.existing(this);
-
-        this.worldBounds = worldBounds;
-        this.setScale(0.3, 0.3);
-        this.scene = scene;
-        this.body.setMaxSpeed(200);
-        this.health = 100;
-        this.maxHealth = 100;
+        super(scene, x, y, worldBounds, socketId);
+     
         this.indicator = new PlayerIndicator(this.scene, this.x, this.y, this)
-        this.socketId = socketId;
 
         scene.input.on('pointermove', (pointer) => {
             let { x:x1, y:y1 } = pointer.position;
@@ -28,9 +20,6 @@ class Player extends Phaser.Physics.Arcade.Sprite {
 
         this.btnAcel = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         this.btnBreak = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-
-        this.graphics = this.scene.add.graphics({x: 0, y: 0, add: true});
-        this.zoom = 10;
     }
 
     preUpdate() {
@@ -48,17 +37,7 @@ class Player extends Phaser.Physics.Arcade.Sprite {
             this.shake();
         }
 
-        if (this.health <= 0) {
-            this.onDead();
-        }
-
-        this.drawHealth();
-        this.scene.emit('playerPosition', { position: { x: this.x, y: this.y }, health: this.health, player: this.socketId })
-    }
-
-    outOfBounds() {
-        return    this.x < 0 || this.x > this.worldBounds.width
-               || this.y < 0 || this.y > this.worldBounds.height
+        this.scene.emit('playerPosition', { position: { x: this.x, y: this.y }, health: this.health, player: this.socketId });
     }
 
     shake() {
@@ -71,39 +50,6 @@ class Player extends Phaser.Physics.Arcade.Sprite {
             this.scene.cameras.main.flashEffect.start(1000, 50, 0.5, 0.5, 0.01)
         }
     }
-
-    drawHealth(){
-        this.graphics.clear();
-        this.graphics.fillStyle(0x990000)
-        this.graphics.fillRectShape({
-            x: this.x - (this.maxHealth /2),
-            y: this.y + 50,
-            width: 100,
-            height: 10 
-        });
-
-        this.graphics.fillStyle(0x009900)
-        this.graphics.fillRectShape({
-            x: this.x - (this.maxHealth /2),
-            y: this.y + 50,
-            width: this.health,
-            height: 10 
-        });
-    }
-
-    onDead() {
-        let newX = Math.floor(Math.random() * (this.worldBounds.width + 1));
-        let newY = Math.floor(Math.random() * (this.worldBounds.height + 1));
-
-        this.health = 100;
-
-        this.x = newX;
-        this.y = newY;
-
-        this.setAcceleration(0);
-        this.setVelocity(0);
-    }
-
 }
 
 module.exports = {
---
+++ src/classes/RemotePlayer.js
@@ -0,0 +1,19 @@
+const Phaser = require('phaser');
+const { IPlayer } = require('./IPlayer')
+const { PlayerIndicator } = require('./PlayerIndicator');
+ 
+class RemotePlayer extends IPlayer {
+    constructor(scene, x, y, worldBounds, socketId) {
+        super(scene, x, y, worldBounds, socketId);
+     
+        this.indicator = new PlayerIndicator(this.scene, this.x, this.y, this)
+    }
+
+    preUpdate() {
+        
+    }
+}
+
+module.exports = {
+    Player
+}
